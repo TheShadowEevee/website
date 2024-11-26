71924529252ec47c01f61e420d64ea8637829552
TheShadowEevee <theshadoweeveeyt@gmail.com>
2024-11-26 09:02:13 -0600

    Package updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+import playformCompress from "@playform/compress";
+import opengraphImages from "astro-opengraph-images";
 
+// playformCompress() should be last (See https://github.com/PlayForm/Compress#readme)
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), opengraphImages(), playformCompress()],
   output: "server",
   adapter: vercel()
 });
